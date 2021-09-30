6261bdaf90175d27c8306061e6bdc4947597ee9b
Mats Rooth <mr249@cornell.edu>
2021-09-30 17:46:10 -0400

    Update arguments in display_ls3_token.

--- display_ls3_token.m
+++ display_ls3_token.m
@@ -1,4 +1,4 @@
-function display_ls3_token(tokenfile,framec)
+function display_ls3_token(tokenfile,matfile,audiodir,framec)
 % Browse tokens in a display where the complete utterance is available.
 % This is a draft of a program that works with current data structures.
 % 
@@ -6,27 +6,40 @@ function display_ls3_token(tokenfile,framec)
 % But fxrapt was not working, it is edited out.
 %
 
-% e.g tokenfile = '/local/res/phon/stress/datar/CANae1_AH0.tok';
+% framec number of frames displayed
+% audiodir location of wav files, one per utterance id
+% matfile mat file encoding data structures
+% tokenfile tokens to display
 
-datfile = '/Volumes/Gray/matlab/matlab-mat/ls3all.mat';
-audiodir = '/Volumes/Gray/matlab/matlab-wav/ls3all';
+% Default arguments.
 
-if nargin < 2
+if nargin < 4
     framec = 150;
 end
 
+% These default files are on MR's external disk Gray.
+
+if nargin < 3
+    audiodir = '/Volumes/Gray/matlab/matlab-wav/ls3all';
+end
+
+if nargin < 2
+    matfile = '/Volumes/Gray/matlab/matlab-mat/ls3all.mat';
+end
 % CANae1_AE1.tok	CANae1_AH0.tok
 
-
 if nargin < 1
-    tokenfile = '/projects/speech/data/phon/stress/datar/HASae1-0.tok'
+    tokenfile = '/local/res/phon/stress/datar/CANae1_AH0.tok';
 end
- 
-% CANae1_AH0.tok'
+
+% Other realization
+% display_ls3_token('/local/res/phon/stress/datar/CANae1_AE1.tok')
+
+% Procedure for creating token file using token-index.awk.
 
 %cat /projects/speech/sys/kaldi-trunk/egs/librispeech3/s5/data/train_clean_100/text | egrep 'THAN I ' | awk -f ../../token-index.awk -v WORD=I > i.tok
     
  
-display_ali_with_token3(datfile,audiodir,tokenfile,framec)
+display_ali_with_token3(matfile,audiodir,tokenfile,framec)
 
 end
